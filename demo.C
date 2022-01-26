d9ee1d20464c41c13594bf7fd08b37801766cd62
shreshthbirla <98342852+shreshthbirla@users.noreply.github.com>
2022-01-26 22:50:50 +0530

    Create demo.C

---
+++ demo.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    int a;
+    printf("enter a number:");
+    scanf("%d",&a);
+    if(a>10)
+    {
+        printf("\n entered number is greater than 10\n");
+    }
+    printf("Thank you!");
+}
